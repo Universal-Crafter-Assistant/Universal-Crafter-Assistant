4e3be4e15a70255046b413b617c80bd53cded469
FaithfulDev <marcelr1989@outlook.com>
2020-09-13 00:11:00 +0200

    prototype 1

--- src/App.js
+++ src/App.js
@@ -1,24 +1,151 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
+import { FlowChartWithState as FlowChart } from "@mrblenny/react-flow-chart";
+import CustomNode from "./CustomNode.js";
+import styled, { css } from "styled-components";
+
 function App() {
+
+  const CanvasOuterCustom = styled.div`
+    position: relative;
+    background-size: 10px 10px;
+    background-color: #a2a2a2;
+    background-image:
+      linear-gradient(90deg,hsla(0,0%,100%,.1) 1px,transparent 0),
+      linear-gradient(180deg,hsla(0,0%,100%,.1) 1px,transparent 0);
+    width: 100%;
+    height: 100%;
+    overflow: hidden;
+    cursor: not-allowed;
+  `
+
+  const chartSimple = {
+    offset: {
+      x: 0,
+      y: 0,
+    },
+    scale: 1,
+    nodes: {
+      sandstone_block: {
+        id: 'sandstone_block',
+        displayName: 'Sandstone Block',
+        amount: 1,
+        source: "Stonemason",
+        crafted: true,
+        position: {
+          x: 425,
+          y: 100,
+        },
+        ports: {
+          port1: {
+            id: 'port1',
+            type: 'output',
+          },
+        },
+      },
+      sandstone: {
+        id: 'sandstone',
+        displayName: "Sandstone",
+        amount: 2,
+        source: "Sandstone",
+        crafted: false,
+        position: {
+          x: 300,
+          y: 250,
+        },
+        ports: {
+          port1: {
+            id: 'port1',
+            type: 'input',
+          },
+        },
+      },
+      limestone_block: {
+        id: 'limestone_block',
+        displayName: "Limestone Block",
+        amount: 1,
+        source: "Stonemason",
+        crafted: true,
+        position: {
+          x: 550,
+          y: 250,
+        },
+        ports: {
+          port1: {
+            id: 'port1',
+            type: 'input',
+          },
+          port2: {
+            id: 'port2',
+            type: 'output',
+          },
+        },
+      },
+      limestone: {
+        id: 'limestone',
+        displayName: 'Limestone',
+        amount: 1,
+        source: "Limestone",
+        crafted: false,
+        position: {
+          x: 425,
+          y: 400,
+        },
+        ports: {
+          port1: {
+            id: 'port1',
+            type: 'input',
+          },
+        },
+      },
+    },
+    links: {
+      link1: {
+        id: 'link1',
+        from: {
+          nodeId: 'sandstone_block',
+          portId: 'port1',
+        },
+        to: {
+          nodeId: 'sandstone',
+          portId: 'port1',
+        },
+      },
+      link2: {
+        id: 'link2',
+        from: {
+          nodeId: 'sandstone_block',
+          portId: 'port1',
+        },
+        to: {
+          nodeId: 'limestone_block',
+          portId: 'port1',
+        },
+      },
+      link3: {
+        id: 'link3',
+        from: {
+          nodeId: 'limestone_block',
+          portId: 'port2',
+        },
+        to: {
+          nodeId: 'limestone',
+          portId: 'port1',
+        },
+      },
+    },
+    selected: {},
+    hovered: {},
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Universal Crafter Assistant</h1>
+      <FlowChart config={{ readonly: true }} initialValue={chartSimple} Components={{
+            NodeInner: CustomNode,
+            CanvasOuter: CanvasOuterCustom
+          }}/>
     </div>
   );
 }
---
+++ src/CustomNode.js
@@ -0,0 +1,51 @@
+import React from "react";
+import styled, { css } from "styled-components";
+
+
+const Outer = styled.div`
+  padding: 15px 15px;
+`
+
+const InnerContainer = styled.div`
+  display: flex;
+  height: 28px;
+  border: 1px solid black;
+  padding: 5px;
+  flex-grow: 1;
+  align-items: center;
+  font-size: 110%;
+`
+
+const Amount = styled.div`
+  width: 15px;
+`
+
+const Name = styled.div`
+  flex: 1;
+`
+
+const CraftedBy = styled.div`
+  border: 1px solid black;
+  border-top: 0px;
+  padding: 5px;
+`
+
+/**
+ * Create the custom component,
+ * Make sure it has the same prop signature
+ */
+const CustomNode = ({ node, config }) => {
+
+    return (
+      <Outer>
+        <InnerContainer>
+          <Amount>{node.amount}</Amount>
+          <Name>{node.displayName}</Name>
+        </InnerContainer>
+        <CraftedBy>{(node.crafted)?"Crafted by:":"Source:"} {node.source}</CraftedBy>
+
+      </Outer>
+    )
+}
+
+export default CustomNode;
